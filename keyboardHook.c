dcd6fdc548546220be6bfb63d8f49feca2b1d1c5
sh-winter <2943331588@qq.com>
2022-07-24 13:55:40 +0800

    初步完成

---
+++ keyboardHook.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <wchar.h>
+#include <stdbool.h>
+#include <Windows.h>
+
+HHOOK hHook;
+
+int main(int argc, char *argv[]) {
+
+  if (argc <= 1) return 1;
+
+  LRESULT CALLBACK MouseHookCallback(int nCode, WPARAM wParam, LPARAM lParam) {
+    KBDLLHOOKSTRUCT *pkh = (KBDLLHOOKSTRUCT *)lParam;
+    if (nCode >= 0) {
+      if (pkh->vkCode == VK_LWIN) {
+        HWND hwnd = GetForegroundWindow();
+        int len = GetWindowTextLengthA(hwnd);
+
+        wchar_t lpString[len];
+        GetWindowTextW(hwnd, lpString, len + 1);
+        char str[len];
+        wcstombs(str, lpString, len + 1);
+
+        bool flag = false;
+        int i;
+        for (i = 1; i < argc; i++) {
+          if (hwnd != NULL) {
+            if (!strcmp(str, argv[i])) {
+              flag = true;
+              break;
+            }
+          }
+        } 
+
+        if (flag) {
+          printf("已拦截指向 \"%s\" 进程的 win left 指令\n", str);
+          return 1;
+        }
+      }
+    }
+    return CallNextHookEx(hHook, nCode, wParam, lParam);
+  }
+
+  hHook = SetWindowsHookExW(WH_KEYBOARD_LL, MouseHookCallback, 0, 0);
+
+  MSG msg;
+  while (GetMessageW(&msg, 0, 0, 0)) {
+    TranslateMessage(&msg); 
+    DispatchMessageW(&msg);
+  }
+}
--- main.ts
+++ main.ts
@@ -1,6 +1,30 @@
 import { cache } from 'https://deno.land/x/cache@0.2.13/mod.ts'
+import { dirname } from "https://deno.land/std@0.149.0/path/mod.ts";
 
-const file = await cache("https://example.com/file.json");
+export async function start(config?: string[]) {
+  if (config) localStorage.setItem('config', JSON.stringify(config))
+  const args = JSON.parse(localStorage.getItem('config') ?? '[]')
 
-const text = await Deno.readTextFile(file.path);
-console.log(text);
+  const { path } = await cache("./keyboardHook.exe")
+
+  const controller = new AbortController()
+  Deno.spawnChild(path, {
+    cwd: dirname(path),
+    args,
+    signal: controller.signal,
+    stdout: 'inherit'
+  })
+
+  return () => controller.abort()
+}
+
+if (import.meta.main) {
+  // const { parse } = await import("https://deno.land/std/flags/mod.ts");
+  
+  const { args } = Deno;
+
+  const config = JSON.parse(localStorage.getItem('config') ?? '[]' );
+  localStorage.setItem('config', JSON.stringify(Array.from(new Set([...config, ...args]))));
+
+  start()
+}
