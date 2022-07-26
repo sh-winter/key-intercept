a04a9bcefea4305fd5672e06d340b3677bd3b834
shenhongbin <shenhongbin@yinhe.ht>
2022-07-26 18:08:56 +0800

    更新处理逻辑，现在不需要写入注册表了

--- main.ts
+++ main.ts
@@ -3,28 +3,33 @@ import { dirname } from "https://deno.land/std@0.149.0/path/mod.ts";
 
 const exeFileURL = import.meta.resolve("./keyboardHook.exe");
 
-export async function start(config?: string[]) {
-  if (config) localStorage.setItem('config', JSON.stringify(config))
-  const args = JSON.parse(localStorage.getItem('config') ?? '[]')
-
-  const existFlag = await exists(exeFileURL);
-  const { path } = await cache(exeFileURL);
-
-  const command = `Add "HKEY_CURRENT_USER\\Software\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers" /v "${path}" /d "WINXPSP3"`
-
-  if (!existFlag) {
-    Deno.spawnSync("reg.exe", {
-      args: [command],
-      stdout: 'inherit'
-    })
+export async function start(newConfig?: string[]) {
+  let config: string[] = [];
+  if (newConfig) {
+    const oldConfig = JSON.parse(localStorage.getItem('config') ?? '[]' ) as string[];
+    config = Array.from(new Set([...oldConfig, ...newConfig]));
+    localStorage.setItem('config', JSON.stringify(config));
+  } else {
+    config = JSON.parse(localStorage.getItem('config') ?? '[]' ) as string[];
   }
 
+  const { path } = await cache(exeFileURL);
+
+  // if (!(await exists(exeFileURL))) {
+  //   // 添加注册表中的程序兼容配置
+  //   const keyName = 'HKEY_CURRENT_USER\\Software\\Microsoft\\Windows NT\\CurrentVersion\\AppCompatFlags\\Layers';
+  //   await Deno.spawn('reg.exe', { args: ['Add', keyName, '/v', path, '/d', 'WINXPSP3', '/f'] });
+  // }
+
+  console.log(`当前配置拦截的应用名:\n    ${config.join('\n    ')}`);
+
   const controller = new AbortController()
-  Deno.spawnChild(path, {
+  Deno.spawn(path, {
     cwd: dirname(path),
-    args,
+    args: config,
     signal: controller.signal,
-    stdout: 'inherit'
+    stdout: 'inherit',
+    stderr: 'inherit'
   })
 
   return () => controller.abort()
@@ -32,11 +37,6 @@ export async function start(config?: string[]) {
 
 if (import.meta.main) {
   // const { parse } = await import("https://deno.land/std/flags/mod.ts");
-  
-  const { args } = Deno;
 
-  const config = JSON.parse(localStorage.getItem('config') ?? '[]' );
-  localStorage.setItem('config', JSON.stringify(Array.from(new Set([...config, ...args]))));
-
-  start()
+  start(Deno.args)
 }
